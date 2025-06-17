5d1431dec573ffef657d67ab1151593db23a867e
Murilo Vieira <murilo1.0@outlook.com>
2025-06-16 22:09:17 -0300

    refactor: enhance product controller with DTO typing and response messages

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import ProductService from "../services/product.service";
+import { CreateProductDTO, UpdateProductDTO } from "../dto/product.dto";
 
 const getAllProducts = async (req: Request, res: Response) => {
   try {
@@ -22,33 +23,39 @@ const getProductById = async (req: Request, res: Response) => {
 
 const registerProduct = async (req: Request, res: Response) => {
   try {
-    const product = req.body;
+    const product: CreateProductDTO = req.body;
     const newProduct = await ProductService.registerProduct(product);
-    res.status(201).json(newProduct);
+    res.status(201).json({message: "Produto Cadastrado com Sucesso!", value: newProduct});
   } catch (error: any) {
     res.status(400).json({ error: error.message });
   }
 };
 
 const deleteProduct = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const deletedProduct = await ProductService.deleteProduct(id);
-        res.status(200).json(deletedProduct);
-    } catch (error: any) {
-        res.status(400).json({ error: error.message });
-    }
-}
-
-const updateProduct = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
-    const product = req.body;
-    const updatedProduct = await ProductService.updateProduct(id, product);
-    res.status(200).json(updatedProduct);
+    const deletedProduct = await ProductService.deleteProduct(id);
+    res.status(200).json({message: "Produto Deletado com Sucesso!", value: deletedProduct});
   } catch (error: any) {
     res.status(400).json({ error: error.message });
   }
 };
 
-export default { registerProduct, deleteProduct, getAllProducts, getProductById, updateProduct };
+const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product: UpdateProductDTO = req.body;
+    const updatedProduct = await ProductService.updateProduct(id, product);
+    res.status(200).json({message: "Produto Atualizado com Sucesso!", value: updatedProduct});
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export default {
+  registerProduct,
+  deleteProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+};
